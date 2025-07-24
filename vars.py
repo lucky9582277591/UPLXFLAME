d50aaab7c5eda35b00e1b2630516af7802805aae
lucky9582277591 <106902554+lucky9582277591@users.noreply.github.com>
2025-07-24 17:41:11 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,8 @@
+# Don't Remove Credit Tg - @Tushar0125
+# Ask Doubt on telegram @Tushar0125
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", "23608428")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "ff78b5440b4cd922b7810fe28abadb73") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "7680377507:AAGSa6f_gs2jg9_lTrevCCtxS8ZtBn_DTI0") #Replace with your bot token
